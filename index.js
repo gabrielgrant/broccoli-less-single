97569d1c200d837ed69d59be349d96e1c9669f73
Jason Mitchell <jasonmit@yahoo-inc.com>
2014-10-10 16:40:52 -0700

    [Enhancement] Adding cache-writer to improve delta performance

--- index.js
+++ index.js
@@ -2,56 +2,55 @@ var fs = require('fs')
 var path = require('path')
 var mkdirp = require('mkdirp')
 var includePathSearcher = require('include-path-searcher')
-var quickTemp = require('quick-temp')
-var mapSeries = require('promise-map-series')
+var CachingWriter = require('broccoli-caching-writer')
 var less = require('less')
 var _ = require('lodash')
 var RSVP = require('rsvp');
 
-module.exports = LessCompiler
+module.exports = LessCompiler;
+LessCompiler.prototype = Object.create(CachingWriter.prototype)
+LessCompiler.prototype.constructor = LessCompiler
+
 function LessCompiler (sourceTrees, inputFile, outputFile, options) {
-  if (!(this instanceof LessCompiler)) return new LessCompiler(sourceTrees, inputFile, outputFile, options)
+  if (!(this instanceof LessCompiler)) {
+    return new LessCompiler(sourceTrees, inputFile, outputFile, options)
+  }
+
+  CachingWriter.apply(this, arguments)
+
   this.sourceTrees = sourceTrees
   this.inputFile = inputFile
   this.outputFile = outputFile
   this.lessOptions = options || {}
 }
 
-LessCompiler.prototype.read = function (readTree) {
-  var self = this
-  quickTemp.makeOrRemake(this, '_tmpDestDir')
-  var destFile = this._tmpDestDir + '/' + this.outputFile
-  mkdirp.sync(path.dirname(destFile))
-  return mapSeries(this.sourceTrees, readTree)
-    .then(function (includePaths) {
-      var lessOptions = {
-        filename: includePathSearcher.findFileSync(self.inputFile, includePaths),
-        paths: includePaths,
+LessCompiler.prototype.updateCache = function (srcDir, destDir) {
+  var destFile = destDir + '/' + this.outputFile
+
+  mkdirp.sync(path.dirname(destFile));
+
+  var lessOptions = {
+    filename: includePathSearcher.findFileSync(this.inputFile, srcDir),
+    paths: srcDir
+  }
+
+  _.merge(lessOptions, this.lessOptions)
+  lessOptions.paths = [path.dirname(lessOptions.filename)].concat(lessOptions.paths);
+  data = fs.readFileSync(lessOptions.filename, 'utf8');
+
+  var parser = new(less.Parser)(lessOptions);
+
+  return new RSVP.Promise(function(resolve, reject) {
+    parser.parse(data, function (err, tree) {
+      if (err) {
+        less.writeError(err, lessOptions);
+        reject(err);
       }
-      _.merge(lessOptions, self.lessOptions)
-      lessOptions.paths = [path.dirname(lessOptions.filename)].concat(lessOptions.paths);
-      data = fs.readFileSync(lessOptions.filename, 'utf8');
 
-      var parser = new(less.Parser)(lessOptions);
+      var css = tree.toCSS(lessOptions);
+      fs.writeFileSync(destFile, css, { encoding: 'utf8' });
 
-      var promise = new RSVP.Promise(function(resolve, reject) {
-        parser.parse(data, function (e, tree) {
-          if (e) {
-            less.writeError(e, lessOptions);
-            reject(e);
-          }
-          var css = tree.toCSS(lessOptions);
-          fs.writeFileSync(destFile, css, { encoding: 'utf8' });
-
-          resolve(self._tmpDestDir);
-        });
-      });
-
-      return promise;
+      resolve();
     });
+  });
 }
-
-LessCompiler.prototype.cleanup = function () {
-  quickTemp.remove(this, '_tmpDestDir')
-}
-
