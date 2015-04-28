08a4fda0916dd3c5f9a61d5e93c328dd6e6d1597
Garth Poitras <garth22@gmail.com>
2015-04-27 23:48:26 -0400

    Restore sourcemap support with less 2.x

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ var less = require('less')
 var _ = require('lodash')
 var RSVP = require('rsvp');
 
+function writeFilePromise(filename, data, options) {
+  return new RSVP.Promise(function(resolve, reject) {
+    fs.writeFile(filename, data, options, function (err) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    });
+  });
+}
+
 module.exports = LessCompiler;
 LessCompiler.prototype = Object.create(CachingWriter.prototype)
 LessCompiler.prototype.constructor = LessCompiler
@@ -18,10 +29,20 @@ function LessCompiler (sourceTrees, inputFile, outputFile, options) {
 
   CachingWriter.apply(this, [arguments[0]].concat(arguments[3]))
 
+  options = options || {};
+  if (options.sourceMap) {
+    if (typeof options.sourceMap !== 'object') {
+      options.sourceMap = {};
+    }
+    if (!options.sourceMap.sourceMapURL) {
+      options.sourceMap.sourceMapURL = outputFile + '.map';
+    }
+  }
+
   this.sourceTrees = sourceTrees
   this.inputFile = inputFile
   this.outputFile = outputFile
-  this.lessOptions = options || {}
+  this.lessOptions = options
 }
 
 LessCompiler.prototype.updateCache = function (srcDir, destDir) {
@@ -40,21 +61,24 @@ LessCompiler.prototype.updateCache = function (srcDir, destDir) {
 
   return new RSVP.Promise(function(resolve, reject) {
 
-    less.render(data, lessOptions)
-      .then(function (output) {
+    less.render(data, lessOptions).then(function (output) {
 
-        fs.writeFile(destFile, output.css, { encoding: 'utf8' }, function (err) {
-          if (err) {
-            return reject(err);
-          }
+      var fileWriterPromises = [ writeFilePromise(destFile, output.css, { encoding: 'utf8' }) ];
+      var sourceMapURL = lessOptions.sourceMap && lessOptions.sourceMap.sourceMapURL;
+      if (sourceMapURL) {
+        fileWriterPromises.push( writeFilePromise(destDir + '/' + sourceMapURL, output.map, { encoding: 'utf8' }) );
+      }
 
-          return resolve(output);
-        });
-
-      }, function (err) {
-        less.writeError(err, lessOptions);
-        reject(err);
+      RSVP.all(fileWriterPromises).then(function() {
+        return resolve(output);
+      }, function(err) {
+        return reject(err);
       });
+
+    }, function (err) {
+      less.writeError(err, lessOptions);
+      reject(err);
+    });
 
   });
 }
