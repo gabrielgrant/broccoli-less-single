6d47d2d26eba0c4fafa557beb755fba606cfdf79
Stefan Penner <stefan.penner@gmail.com>
2015-05-04 10:53:46 -0700

    * improve promise usage * update mocha * add npm run test:debug (for easy debugging)

--- index.js
+++ index.js
@@ -4,21 +4,12 @@ var mkdirp = require('mkdirp')
 var includePathSearcher = require('include-path-searcher')
 var CachingWriter = require('broccoli-caching-writer')
 var less = require('less')
-var _ = require('lodash')
+var merge = require('lodash-node/modern/object/merge')
 var RSVP = require('rsvp');
-
-function writeFilePromise(filename, data, options) {
-  return new RSVP.Promise(function(resolve, reject) {
-    fs.writeFile(filename, data, options, function (err) {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(data);
-    });
-  });
-}
+var writeFile = RSVP.denodeify(fs.writeFile);
 
 module.exports = LessCompiler;
+
 LessCompiler.prototype = Object.create(CachingWriter.prototype)
 LessCompiler.prototype.constructor = LessCompiler
 
@@ -55,30 +46,24 @@ LessCompiler.prototype.updateCache = function (srcDir, destDir) {
     paths: srcDir
   }
 
-  _.merge(lessOptions, this.lessOptions)
+  merge(lessOptions, this.lessOptions)
+
   lessOptions.paths = [path.dirname(lessOptions.filename)].concat(lessOptions.paths);
   data = fs.readFileSync(lessOptions.filename, 'utf8');
 
-  return new RSVP.Promise(function(resolve, reject) {
-
-    less.render(data, lessOptions).then(function (output) {
-
-      var fileWriterPromises = [ writeFilePromise(destFile, output.css, { encoding: 'utf8' }) ];
+  return less.render(data, lessOptions).
+    catch(function(err) {
+      less.writeError(err, lessOptions);
+      throw err;
+    }).
+    then(function (output) {
+      var fileWriterPromises = [writeFile(destFile, output.css, { encoding: 'utf8' }) ];
       var sourceMapURL = lessOptions.sourceMap && lessOptions.sourceMap.sourceMapURL;
+
       if (sourceMapURL) {
-        fileWriterPromises.push( writeFilePromise(destDir + '/' + sourceMapURL, output.map, { encoding: 'utf8' }) );
+        fileWriterPromises.push( writeFile(destDir + '/' + sourceMapURL, output.map, { encoding: 'utf8' }) );
       }
 
-      RSVP.all(fileWriterPromises).then(function() {
-        return resolve(output);
-      }, function(err) {
-        return reject(err);
-      });
-
-    }, function (err) {
-      less.writeError(err, lessOptions);
-      reject(err);
-    });
-
-  });
+      return RSVP.Promise.all(fileWriterPromises);
+    })
 }
